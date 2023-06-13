f4d426239346b87ebe21f296e2b8bed4db9e2380
Scifff <sanek.bit13@mail.ru>
2023-06-14 02:31:24 +0300

    ready

--- script.js
+++ script.js
@@ -1,149 +1,142 @@
-const DATA = [
+const questions = [
     {
-        questions: 'aaaaaa1',
-        answers: [
-            {
-                id: '0',
-                value: 'aaaaa1',
-                correct: true,
-            },
-            {
-                id: '1',
-                value: 'aaaaa2',
-                correct: false,
-            },
-            {
-                id: '2',
-                value: 'aaaaa3',
-                correct: false,
-            },
-        ]
+        question: "How many planets are in the solar system?",
+        answers: [8, 9, 10],
+        correct: 1,
     },
     {
-        questions: 'aaaaaa2',
-        answers: [
-            {
-                id: '3',
-                value: 'aaaaa4',
-                correct: false,
-            },
-            {
-                id: '4',
-                value: 'aaaaa5',
-                correct: true,
-            },
-            {
-                id: '5',
-                value: 'aaaaa6',
-                correct: false,
-            },
-        ]
-    }
-]
+        question: "What is the freezing point of water?",
+        answers: [0, -5, -6],
+        correct: 1,
+    },
+    {
+        question: "What is the longest river in the world?",
+        answers: ["Nile", "Amazon", "Yangtze"],
+        correct: 1,
+    },
+    {
+        question: "How many chromosomes are in the human genome?",
+        answers: [42, 44, 46],
+        correct: 3,
+    },
+    {
+        question: "What is the capital of Canada?",
+        answers: ["Toronto", "Ottawa", "Vancouver"],
+        correct: 2,
+    },
+    {
+        question: "What is the Jewish New Year called?",
+        answers: ["Hanukkah", "Yom Kippur", "Kwanzaa"],
+        correct: 1,
+    },
+];
+//Находим эл-ты
+const headerContainer = document.querySelector('#header');
+const listContainer = document.querySelector('#list');
+const submitBtn = document.querySelector('#submit');
 
-let localResults = {};
+//переменные
+let score = 0;
+let questionIndex = 0;
 
-const quiz = document.getElementById('quiz');
-const questions = document.getElementById('questions');
-const results = document.getElementById('results');
-const indicator = document.getElementById('indicator');
-const btnNext = document.getElementById('btn-next');
-const btnRestart = document.getElementById('btn-restart');
-
-const renderQuestions = (index) => {
-    renderIndicator(index + 1);
-
-    questions.dataset.currentStep = index;
-
-    const renderAnswers = () => DATA[index].answers
-        .map((answers) => `
-            <li>
-                <label>
-                    <input class="answers-input" type="radio" name = ${index} value =${answers.id}>
-                    ${answers.value}
-                </label>
-            </li>
-        `)
-        .join('');
-    
-    
-    questions.innerHTML = `
-        <div class="quizQuestionsItem">
-                <div class="quizQuestionsItem__questions">${DATA[index].questions}</div>
-                <ul class="quizQuestionsItem__answers">${renderAnswers()}</ul>
-        </div>
-    `;
+clearPage();
+showQuestion();
+submitBtn.onclick = checkAnswer;
+//чистим страницу
+function clearPage(){
+    headerContainer.innerHTML = '';
+    listContainer.innerHTML = '';
 };
 
-const renderResults = () => {
-    let content = '';
-
-    const getClassname = (answers, questionsIndex) => {
-        let classname = '';
-        if (!answers.correct && answers.id === localResults[questionsIndex]) {
-            classname = 'answerInvalid';
-        } else if (answers.correct) {
-            classname = 'answerValid';
-        }
-        return classname;
+function showQuestion(){
+    console.log('showQuestion');
+//вопрос   
+    const headerTemplate = `<h2 class="title">%title%</h2>`;
+    const title = headerTemplate.replace('%title%', questions[questionIndex]['question']);
+    headerContainer.innerHTML = title;
+//ответ
+    let answerNumber = 1;
+    for (answerText of questions[questionIndex]['answers']) {
+        const questionTemplate = 
+        `<li>
+            <label>
+                <input value="%number%" type="radio" class="answer" name="answer">
+                <span>%answer%</span>
+            </label>
+        </li>`;
+        const answerHTML = questionTemplate
+                            .replace('%answer%', answerText)
+                            .replace('%number%', answerNumber)
+        listContainer.innerHTML += answerHTML;
+        answerNumber++;
     }
+}
 
-    const getAnswers = (questionsIndex) =>  DATA[questionsIndex].answers
-        .map((answers) =>  `<li>${getClassname(answers, questionsIndex)}>${answers.value}</li>`)
-        .join('');  
+function checkAnswer(){
+    console.log('aaaa');
+//выбран ли ответ
+    const checkedRadio = listContainer.querySelector('input[type = "radio"]:checked')
+//если ответа нет ничего не делаем
+    if (!checkedRadio) {
+        submitBtn.blur();
+        return
+    }
+    //узнаем номер ответа пользователя
+    const userAnswer = parseInt(checkedRadio.value)
+//если верно +счет
+    if (userAnswer === questions[questionIndex]['correct']) {
+        score++;
+    }
+    console.log('score', score);
+//последний или нет
+    if (questionIndex !== questions.length - 1){
+        console.log('NotLast');
+        questionIndex++;
+        clearPage();
+        showQuestion();
+        return;
+    } else {
+        console.log('Last');
+        clearPage();
+        showResults();
+    }
+}
+//результаты
+function showResults() {
+    console.log('showResults start');
+    console.log(score);
 
-    DATA.forEach((questions, index) =>{
-        content += `
-        <div class="quizResultsItem">
-            <div class="quizResultsItem__question">${questions.questions}</div>
-            <ul class="quizResultsItem__answers">${getAnswers(index)}</ul>
-        </div>
+    const resultsTemplate = `
+        <h2 class="title">%title%</h2>
+        <h3 class="summary">%message%</h3>
+        <p class="result">%result%</p>
         `;
-    });
+    
+        let title, message;
 
-    results.innerHTML = content;
-};
-
-const renderIndicator = (currentStep) => {
-    indicator.innerHTML = `${currentStep}/${DATA.length}`;
-};
-
-quiz.addEventListener('change', (event) => {
-    if (event.target.classList.contains('answers-input')){
-        localResults[event.target.name] = event.target.value;
-        btnNext.disabled = false;
-    }
-});
-
-quiz.addEventListener('click', (event) => {
-    if (event.target.classList.contains('btn-next')){
-        const nextQuestionsIndex = Number(questions.dataset.currentStep) + 1;
-        
-        if (DATA.length === nextQuestionsIndex) {
-            questions.classList.add('questionsHidden');
-            results.classList.add('resultsHidden');
-            indicator.classList.add('indicatorVisible');
-            btnNext.classList.add('btnNextHidden');
-            btnRestart.classList.add('btnRestartVisible');
-            renderResults();
+        if (score === questions.length) {
+            title = 'Victory!';
+            message = 'You answered all the questions correctly';
+        } else if ((score * 100) / questions.length >= 50) {
+            title = 'Not bad, not bad';
+            message = 'You answered more than half of the questions correctly';
         } else {
-            renderQuestions(nextQuestionsIndex);
+            title = '😥';
+            message = 'You have less than half of the correct answers';
+
         }
-        btnNext.disabled = true;
-    }
 
-    if (event.target.classList.contains('btn-restart')){
-        localResults = {};
-        results.innerHTML = '';
+        let result = `${score} / ${questions.length}`;
 
-        questions.classList.remove('questionsHidden');
-        results.classList.remove('resultsHidden');
-        indicator.classList.remove('indicatorVisible');
-        btnNext.classList.remove('btnNextHidden');
-        btnRestart.classList.remove('btnRestartVisible');
 
-        renderQuestions(0);
-    }
-});
+        const finalMessage = resultsTemplate
+                                    .replace('%title%', title)
+                                    .replace('%message%', message)
+                                    .replace('%result%', result)
+        headerContainer.innerHTML = finalMessage;
 
-renderQuestions(0);
+        //рестарт
+        submitBtn.blur();
+        submitBtn.innerText = 'Restart';
+        submitBtn.onclick = () =>{history.go()};
+}
