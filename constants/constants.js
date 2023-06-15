44263c7540ed5585587276bd184d4fcf9a2b8e43
Scifff <sanek.bit13@mail.ru>
2023-06-15 21:29:30 +0300

    fix

---
+++ constants/constants.js
@@ -0,0 +1,33 @@
+const questions = [
+    {
+        question: "How many planets are in the solar system?",
+        answers: [8, 9, 10],
+        correct: 1,
+    },
+    {
+        question: "What is the freezing point of water?",
+        answers: [0, -5, -6],
+        correct: 1,
+    },
+    {
+        question: "What is the longest river in the world?",
+        answers: ["Nile", "Amazon", "Yangtze"],
+        correct: 1,
+    },
+    {
+        question: "How many chromosomes are in the human genome?",
+        answers: [42, 44, 46],
+        correct: 3,
+    },
+    {
+        question: "What is the capital of Canada?",
+        answers: ["Toronto", "Ottawa", "Vancouver"],
+        correct: 2,
+    },
+    {
+        question: "What is the Jewish New Year called?",
+        answers: ["Hanukkah", "Yom Kippur", "Kwanzaa"],
+        correct: 1,
+    },
+];
+
--- script.js
+++ script.js
@@ -1,60 +1,33 @@
-const questions = [
-    {
-        question: "How many planets are in the solar system?",
-        answers: [8, 9, 10],
-        correct: 1,
-    },
-    {
-        question: "What is the freezing point of water?",
-        answers: [0, -5, -6],
-        correct: 1,
-    },
-    {
-        question: "What is the longest river in the world?",
-        answers: ["Nile", "Amazon", "Yangtze"],
-        correct: 1,
-    },
-    {
-        question: "How many chromosomes are in the human genome?",
-        answers: [42, 44, 46],
-        correct: 3,
-    },
-    {
-        question: "What is the capital of Canada?",
-        answers: ["Toronto", "Ottawa", "Vancouver"],
-        correct: 2,
-    },
-    {
-        question: "What is the Jewish New Year called?",
-        answers: ["Hanukkah", "Yom Kippur", "Kwanzaa"],
-        correct: 1,
-    },
-];
-//Находим эл-ты
+
 const headerContainer = document.querySelector('#header');
 const listContainer = document.querySelector('#list');
-const submitBtn = document.querySelector('#submit');
+const submitButton = document.querySelector('#submit');
+const indicator = document.getElementById('indicator');
 
-//переменные
+
 let score = 0;
 let questionIndex = 0;
+let currentQuestion = 1;
 
 clearPage();
 showQuestion();
-submitBtn.onclick = checkAnswer;
-//чистим страницу
+
+
+submitButton.onclick = checkAnswer;
+
 function clearPage(){
     headerContainer.innerHTML = '';
     listContainer.innerHTML = '';
 };
 
+
+
 function showQuestion(){
-    console.log('showQuestion');
-//вопрос   
+ 
     const headerTemplate = `<h2 class="title">%title%</h2>`;
     const title = headerTemplate.replace('%title%', questions[questionIndex]['question']);
     headerContainer.innerHTML = title;
-//ответ
+
     let answerNumber = 1;
     for (answerText of questions[questionIndex]['answers']) {
         const questionTemplate = 
@@ -68,43 +41,38 @@ function showQuestion(){
                             .replace('%answer%', answerText)
                             .replace('%number%', answerNumber)
         listContainer.innerHTML += answerHTML;
+
+        indicator.innerHTML = `${currentQuestion}/${questions.length}`
+
         answerNumber++;
     }
-}
+    };
 
 function checkAnswer(){
-    console.log('aaaa');
-//выбран ли ответ
     const checkedRadio = listContainer.querySelector('input[type = "radio"]:checked')
-//если ответа нет ничего не делаем
     if (!checkedRadio) {
-        submitBtn.blur();
+        submitButton.blur();
         return
     }
-    //узнаем номер ответа пользователя
+
     const userAnswer = parseInt(checkedRadio.value)
-//если верно +счет
+
+    currentQuestion++;
     if (userAnswer === questions[questionIndex]['correct']) {
         score++;
     }
-    console.log('score', score);
-//последний или нет
     if (questionIndex !== questions.length - 1){
-        console.log('NotLast');
         questionIndex++;
         clearPage();
         showQuestion();
         return;
     } else {
-        console.log('Last');
         clearPage();
         showResults();
     }
 }
-//результаты
 function showResults() {
-    console.log('showResults start');
-    console.log(score);
+    indicator.classList.add('nonVisible');
 
     const resultsTemplate = `
         <h2 class="title">%title%</h2>
@@ -117,7 +85,7 @@ function showResults() {
         if (score === questions.length) {
             title = 'Victory!';
             message = 'You answered all the questions correctly';
-        } else if ((score * 100) / questions.length >= 50) {
+        } else if (score >= 3) {
             title = 'Not bad, not bad';
             message = 'You answered more than half of the questions correctly';
         } else {
@@ -129,14 +97,14 @@ function showResults() {
         let result = `${score} / ${questions.length}`;
 
 
+
         const finalMessage = resultsTemplate
                                     .replace('%title%', title)
                                     .replace('%message%', message)
                                     .replace('%result%', result)
         headerContainer.innerHTML = finalMessage;
 
-        //рестарт
-        submitBtn.blur();
-        submitBtn.innerText = 'Restart';
-        submitBtn.onclick = () =>{history.go()};
+        submitButton.blur();
+        submitButton.innerText = 'Restart';
+        submitButton.onclick = () =>{history.go()};
 }
